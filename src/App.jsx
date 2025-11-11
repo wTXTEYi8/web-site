8d9460eeb682d6ed83c01af858c137259085d316
wTXTEYi8 <0tm2wy52729112g@ezweb.ne.jp>
2025-11-12 06:30:01 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function MyButton() {
+  return (
+    <button>
+      ボタン
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>ようこそ</h1>
+      <MyButton />
+    </div>
+  );
+}
+
